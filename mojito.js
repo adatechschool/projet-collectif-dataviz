853dc8eebc7cbf070414e12adc9b47dd3aec0077
TimotheeRoy <timroy93100@gmail.com>
2023-11-22 12:36:29 +0100

    affichage ingredients

--- mojito.js
+++ mojito.js
@@ -1,17 +1,31 @@
-document.addEventListener("DOMContentLoaded", function(){ //quand on charge les éléments du DOM alors ça fait ça
+document.addEventListener("DOMContentLoaded", function () { //quand on charge les éléments du DOM alors ça fait ça
     const apiURL = "http://thecocktaildb.com/api/json/v1/1/search.php?s=mojito" //à réutiliser en changeant le nom du cocktail
     fetch(apiURL).then(rep => rep.json()).then(data => {
         const name = document.querySelector("#name")
         const ingredients = document.querySelector("#Ingrédients")
         const instructions = document.querySelector("#Instructions")
-    
-        const drinkName = data.drinks[0].strDrink 
-        console.log('drinkname' , drinkName);
+
+        const drinkName = data.drinks[0].strDrink
         name.innerHTML = `${drinkName}`
-    
+
+       
+        const mojitoObj = data.drinks[0]
+        for (let key in mojitoObj){
+            console.log('key :', key)
+            if (mojitoObj.hasOwnProperty(key)){
+                if (key.includes('strIngredient') && mojitoObj[key] !== null){
+                    console.log(mojitoObj.key)
+                    ingredients.innerHTML += `<li> ${mojitoObj[key]} </li>`
+                }
+                if (key.includes('strDrink')){
+
+                }
+                
+            }
+        }
     }
 
-        )
+    )
 
 
 
