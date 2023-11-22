2abb46b37de2fffc08ec20ef99a898c660847b8e
TimotheeRoy <timroy93100@gmail.com>
2023-11-22 14:19:23 +0100

    correction du clean

--- mojito.js
+++ mojito.js
@@ -1,24 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () { //quand on charge les éléments du DOM alors ça fait ça
     const apiURL = "http://thecocktaildb.com/api/json/v1/1/search.php?s=mojito" //à réutiliser en changeant le nom du cocktail
     fetch(apiURL).then(rep => rep.json()).then(data => {
+
         const name = document.querySelector("#name")
         const ingredients = document.querySelector("#Ingrédients")
         const instructions = document.querySelector("#Instructions")
 
-        const drinkName = data.drinks[0].strDrink
-        name.innerHTML = `${drinkName}`
-
-       
         const mojitoObj = data.drinks[0]
-        for (let key in mojitoObj){
-            if (key.includes('strIngredient') && mojitoObj[key] !== null)
-                ingredients.innerHTML += `<li> ${mojitoObj[key]} </li>`
-                
-            if (key.includes('strDrink'))
-                name.innerHTML += ``
 
-            if (key.includes(''))
-                
+        for (let key in mojitoObj) {
+            if (mojitoObj.hasOwnProperty(key)) {
+                if (key.includes('strIngredient') && mojitoObj[key] !== null)
+                    ingredients.innerHTML += `<li> ${mojitoObj[key]} </li>`
+
+                if (key.includes('strDrink'))
+                    name.innerHTML = `${mojitoObj.strDrink}`
+
+                if (key.includes(''))
+                    instructions.innerHTML += ``
+            }
         };
     });
 });
